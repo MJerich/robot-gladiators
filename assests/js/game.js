94a6fb55e6438eab0a0dcd1df0c8b7329418c144
MJerich <MatthewJerich@gmail.com>
2022-02-13 13:11:22 -0700

    added initial battle and if statements to check health

--- assests/js/game.js
+++ assests/js/game.js
@@ -1,1 +1,51 @@
-window.alert("This is an alert! JavaScript is running!");
+// set our player variables
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+// log our variables for accuracy
+console.log(playerName, playerAttack, playerHealth);
+
+// setup enemys variables
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    // Alert the players we are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+
+    // Subtract the value of 'playerAttack' from the value of 'enemyHealth'
+    enemyHealth = enemyHealth - playerAttack;
+
+    // log result to the console
+    console.log(
+        playerName + " attacked" + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+
+    // Check ememys health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+    // Subtract 'enemyAttack' from 'playerHealth'
+    playerHealth = playerHealth - enemyAttack;
+
+    // Log result
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+
+    // check player's health
+if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+    } 
+    else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+}
+
+fight();
