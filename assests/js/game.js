ee895d5d118426ca43038cd5676daf653fcba3c8
MJerich <MatthewJerich@gmail.com>
2022-02-15 13:01:14 -0700

    multiple rounds complete

--- assests/js/game.js
+++ assests/js/game.js
@@ -58,7 +58,12 @@ var fight = function(enemyName) {
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        fight(pickedEnemyName);
+    }
+    else {
+        window.alert("You have lost your robot in battle. Game Over!")
+    }
 }
